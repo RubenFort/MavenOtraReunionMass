124ae8756e5514e4f1c9a2c5f1409f296625ae61
Ruben <rdfortu@gmail.com>
2021-07-19 01:27:06 +0200

    Limpiar y odenar codigo

--- src/main/java/com/linkedin/learning/otraReunionMas/App.java
+++ src/main/java/com/linkedin/learning/otraReunionMas/App.java
@@ -23,80 +23,86 @@ import com.linkedin.learning.otraReunionMas.dominio.Sala;
 
 import jdk.nashorn.internal.objects.NativeWeakMap;
 
-public class App 
-{
-    public static void main( String[] args ) throws SQLIntegrityConstraintViolationException
-    {
-        ReunionDao reunionDao = new ReunionDao();
-        List<Reunion> reuniones = reunionDao.getAll();
-        System.out.println(reuniones);
-        
-        Persona juan = new Persona("E003f", "Juan", "López");
-        Persona ana = new Persona("E004f", "Ana", "Gómez");
-        
-        Reunion reunion = new Reunion(LocalDateTime.now(), "Test");
-        System.out.println(reunion);
-        reunion.addParticipante(juan);
-        reunion.addParticipante(ana);
-        
-        reunionDao.save(reunion);
+public class App {
+	public static void main(String[] args) throws SQLIntegrityConstraintViolationException {
+		
+		// DAOs
+		ReunionDao reunionDao = new ReunionDao();
+		ActaDao actaDao = new ActaDao();
+		SalaDao salaDao = new SalaDao();
 
-        reunionDao.save(reunion);
-        System.out.println(reunion);
-        
-        ActaDao actaDao = new ActaDao();
-        Acta acta = new Acta("Reunion anulada", reunion);
-        actaDao.save(acta);
-        
-        Reunion reunion1 = new Reunion(LocalDateTime.now(),"Otra reunión de Test");
-        juan.addReunion(reunion1);
-        reunionDao.save(reunion1);
-        
-        //Date dt = new Date();
-        //Date tomorrow2 = new Date(dt.getTime() + (1000 * 60 * 60 * 24)*2);
-        Reunion reunion2 = new Reunion(LocalDateTime.now().plus(2, ChronoUnit.DAYS), "Reunión de pasado mañana");
-        System.out.println(reunion2);
-        reunionDao.save(reunion2);
-        try {
-        	System.out.println("Tu próxima reunión es " + reunionDao.proximaReunion());
+		// Creacion de objetos
+		Sala s099 = new Sala("S099", "Trastero", 1);
+		Sala s101 = new Sala("S101", "Reunión primera planta", 10);
+		Sala s109 = new Sala("S109", "Entrevistas primera planata", 3);
+		Sala s203 = new Sala("S203", "Sala grande", 25);
+
+		salaDao.save(s099);
+		salaDao.save(s101);
+		salaDao.save(s109);
+		salaDao.save(s203);
+
+		Persona marta = new Persona("E001", "Marta", "García López");
+		Persona pedro = new Persona("E002", "Pedro", "Gómez Fernandez");
+		Persona santi = new Persona("E003", "Santi", "Leis Pérez");
+		Persona luisa = new Persona("E004", "Luisa", "Vara González");
+
+		Reunion r0 = new Reunion(LocalDateTime.now(), "Reunión de Test");// Reunion posee ids autogenerados
+		Reunion r1 = new Reunion(LocalDateTime.now().plus(2, ChronoUnit.HOURS), "Otra reunión de Test");
+		Reunion r2 = new Reunion(LocalDateTime.now().plus(2, ChronoUnit.DAYS), "Reunión de pasado mañana");
+		Reunion r3 = new Reunion(LocalDateTime.now().plus(1, ChronoUnit.DAYS), "Reunión de mañana");
+		Reunion r4 = new Reunion(LocalDateTime.now().minus(1, ChronoUnit.DAYS), "Reunión de ayer");
+
+		r0.addParticipante(marta);
+		r0.setSala(s099);
+		reunionDao.save(r0);
+		Acta a0 = new Acta("Marta se reúne sola, solo para descansar un rato", r0);
+		actaDao.save(a0);
+		reunionDao.update(r0);
+
+		r1.addParticipante(marta);
+		r1.addParticipante(pedro);
+		r1.addParticipante(santi);
+		r1.addParticipante(luisa);
+		r1.setSala(s101);
+		reunionDao.save(r1);
+
+		r2.addParticipante(pedro);
+		r2.addParticipante(santi);
+		r2.setSala(s109);
+		reunionDao.save(r2);
+
+		r3.addParticipante(marta);
+		r3.addParticipante(luisa);
+		r3.setSala(s109);
+		reunionDao.save(r3);
+
+		r4.addParticipante(marta);
+		r4.addParticipante(pedro);
+		r4.addParticipante(santi);
+		r4.addParticipante(luisa);
+		r4.setSala(s203);
+		reunionDao.save(r4);
+
+		Acta a4 = new Acta("Participantes: M.García, P.Gómez, S.Leis, L.Vara. Duracion: "
+				+ "1h. Se reunieron en la sala 203 para preparar el lanzamiento de la aplicación "
+				+ "\"Otra Reunión Más\".", r4);
+
+		actaDao.save(a4);
+		reunionDao.update(r4);
+
+		// Recuperación de datos
+		List<Reunion> reuniones = reunionDao.getAll();
+		System.out.println("Todas las reuniones: " + reuniones);
+
+		try {
+			System.out.println("Tu próxima reunión es: " + reunionDao.proximaReunion());
 		} catch (NoResultException e) {
 			System.out.println("No tienes ninguna reunión a la vista");
 		}
+
+		List<Reunion> reunionesManhana = reunionDao.reunionesManana();
+		System.out.println("Para mañana: " + reunionesManhana);
         
-        Reunion reunion3 = new Reunion(LocalDateTime.now().plus(1, ChronoUnit.DAYS), "Reunion de mañana");
-        reunionDao.save(reunion3);
-        List<Reunion> reunionesManana = reunionDao.reunionesManana();
-        System.out.println("Para mañana: " + reunionesManana);
-        
-        SalaDao salaDao = new SalaDao();
-        
-        System.out.println("****************************************************");
-        System.out.println("****************************************************");
-        
-        Sala sala = new Sala("206", "Sala principal conferencias realeza", 2000);
-        
-        try {
-        	salaDao.save(sala);
-		} catch (Exception e) {
-			System.out.println("Ya existe una sala con el mismo id");
-		}
-        
-        System.out.println("Paso 1 " + salaDao.getAll());
-        
-        sala.setDescription("Sala principal reformada");
-        salaDao.update(sala);
-        
-        System.out.println("Paso 2 " + salaDao.getAll());
-        
-        Sala sala2 = new Sala("45", "Sala de meditación", 1);
-        
-        salaDao.save(sala2);
-        
-        System.out.println("Paso 3 " + salaDao.getAll());
-        
-        salaDao.delete(sala2);
-        
-        System.out.println("Paso 4 " + salaDao.getAll());
-        
-    }
+	}
 }
